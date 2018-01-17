d3fc6efba99fca83948913b4b870879f48af763b
loveKino <842914439@qq.com>
2018-01-17 16:51:03 +0800

    support commands

--- skeleton/skeleton.go
+++ skeleton/skeleton.go
@@ -2,8 +2,11 @@ package skeleton
 
 import (
 	"../util"
+	"fmt"
 	"log"
+	"os/exec"
 	"path/filepath"
+	"strings"
 )
 
 // instance of skeleton
@@ -26,16 +29,35 @@ type SkeletonCommand struct {
 	After  string `json:"after"`
 }
 
+const (
+	DEFAULT_TPL_DIR_NAME    = "stpl"
+	DEFAULT_TARGET_DIR_NAME = "skel_target"
+)
+
 func BuildSkelton(configFilePath string) {
 	skeletonConfig := readSkeletonConfig(configFilePath)
+	skeletonPath := getSkeletonClassPath(configFilePath, skeletonConfig.SkeletonPath)
+	targetRoot := getTargetRootPath(configFilePath, skeletonConfig.TargetPath)
 
-	skeletonPath := filepath.Join(filepath.Dir(configFilePath), skeletonConfig.SkeletonPath)
+	RunSkeleton(skeletonPath, targetRoot, skeletonConfig.Context)
+}
+
+func RunSkeleton(skeletonPath string, targetRoot string, context map[string]interface{}) {
 	skeletonClass := readSkeletonClass(skeletonPath)
 
-	targetRoot := filepath.Join(filepath.Dir(configFilePath), skeletonConfig.TargetPath)
-	skeletonTplPath := filepath.Join(filepath.Dir(skeletonPath), skeletonClass.TemplateDir)
+	skeletonTplPath := getSkeletonTemplateDirPath(skeletonPath, skeletonClass.TemplateDir)
+
+	// run before command
+	if skeletonClass.Commands.Before != "" {
+		log.Print("[gs-skeleton] run command :" + skeletonClass.Commands.Before)
+		if err := runCommand(skeletonClass.Commands.Before, ""); err != nil {
+			panic(err)
+		}
+	}
+
+	// template
 	log.Print("[gs-skeleton] build template from dir:" + skeletonTplPath)
-	buildErr := BuildTemplate(skeletonTplPath, targetRoot, skeletonConfig.Context, TemplateOptions{
+	buildErr := BuildTemplate(skeletonTplPath, targetRoot, context, TemplateOptions{
 		Ignore: skeletonClass.Ignore,
 	})
 
@@ -43,6 +65,56 @@ func BuildSkelton(configFilePath string) {
 		log.Print("[gs-skeleton] fail to build template from :" + skeletonPath)
 		panic(buildErr)
 	}
+
+	// run after command
+	if skeletonClass.Commands.After != "" {
+		log.Print("[gs-skeleton] run command :" + skeletonClass.Commands.After)
+		if err := runCommand(skeletonClass.Commands.After, targetRoot); err != nil {
+			panic(err)
+		}
+	}
+
+}
+
+func runCommand(cmdStr string, dir string) error {
+	cmd := exec.Command("bash", "-c", cmdStr)
+	if dir != "" {
+		cmd.Dir = dir
+	}
+
+	out, err := cmd.Output()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%s", out)
+
+	return nil
+}
+
+// using default if not provide
+func getTargetRootPath(configFilePath, targetPath string) string {
+	tarP := strings.TrimSpace(targetPath)
+	if tarP == "" {
+		tarP = DEFAULT_TARGET_DIR_NAME
+	}
+	return filepath.Join(filepath.Dir(configFilePath), tarP)
+}
+
+// using default tpl name if not provide
+func getSkeletonTemplateDirPath(skeletonClassPath, templateDir string) string {
+	tplDir := strings.TrimSpace(templateDir)
+	if tplDir == "" {
+		tplDir = DEFAULT_TPL_DIR_NAME
+	}
+	return filepath.Join(filepath.Dir(skeletonClassPath), tplDir)
+}
+
+// TODO default global directory
+// require level
+// @return the absolute file path for skeleton
+// TODO download if not exists
+func getSkeletonClassPath(configFilePath, skeletonPath string) string {
+	return filepath.Join(filepath.Dir(configFilePath), skeletonPath)
 }
 
 // TODO check config
